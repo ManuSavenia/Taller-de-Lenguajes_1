4ab2c480d05d054478abacd9fd8653882cecf53e
Manuel <saveniamanuel@gmail.com>
2023-06-06 18:21:39 -0300

    arreglado

--- Mio/Punto8ParcialRepaso.c
+++ Mio/Punto8ParcialRepaso.c
@@ -1,41 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 typedef struct nodo
 {
     int dato;
     struct nodo *siguiente;
 } lista;
-void agregar(lista *L);
+void agregar(lista **L);
 void eliminar(lista *L);
-void eliminarTodo(lista *L); 
+void eliminarTodo(lista *L);
+void printList(lista *l);
 int main()
 {
-    lista *L = malloc(sizeof(lista));
-    L->siguiente = NULL;
+    lista *L = NULL;
+    agregar(&L);
+    agregar(&L);
+    printList(L);
+    eliminarTodo(L);
     return 0;
 }
 
-void agregar(lista *L)
+void agregar(lista **L)
 {
     int n;
     lista *aux = malloc(sizeof(lista));
     printf("ingrese el dato a agregar: ");
     scanf("%d", &n);
     aux->dato = n;
-    aux->siguiente = L;
-    L = aux;
+    aux->siguiente = *L;
+    *L = aux;
 }
 
 void eliminar(lista *L)
 {
     int n;
+    printf("Ingrese dato a eliminar: ");
+    scanf("%d", n);
     lista *aux = L, *ant = L;
     while (aux != NULL)
     {
-        if (n = aux->dato)
+        if (n == aux->dato)
         {
             if (aux == L)
             {
                 L = L->siguiente;
+                free(aux);
             }
             else
             {
@@ -44,6 +52,8 @@ void eliminar(lista *L)
                 break;
             }
         }
+        ant = aux;
+        aux = aux->siguiente;
     }
 }
 
@@ -55,5 +65,15 @@ void eliminarTodo(lista *L)
         aux = L;
         L = L->siguiente;
         free(aux);
+    }
+}
+
+void printList(lista *l)
+{
+    lista *aux = l;
+    while (aux != NULL)
+    {
+        printf("%d ", aux->dato);
+        aux = aux->siguiente;
     }
 }
